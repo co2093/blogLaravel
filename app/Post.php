3f79130de9ae3e1e04613d87c2189e1f38923ddc
Omar <coh.2093@gmail.com>
2020-08-20 23:07:43 -0600

    seeders

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+
+	public function tags(){
+		return $this->belongsToMany(Tag::class);
+	}
+
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(App\User::class, 29)->create();
+        App\User::create(
+
+        	[
+
+        		'name' => 'Omar Hernandez',
+        		'email' => 'o.@omar.com',
+        		'password' => bcrypt('1234')
+
+        	]
+
+        );
+
+    }
+}

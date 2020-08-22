3d90ad95f342cdc84035d9f1a7c721362c327647
Omar <coh.2093@gmail.com>
2020-08-21 22:18:16 -0600

    Lista de articulos

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class PageController extends Controller
 {
     public function blog(){
 
-    	return view ('web.posts');
+    	$posts = Post::orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(3);
+
+
+    	return view ('web.posts', compact('posts'));
 
     }
 }
--- resources/views/web/posts.blade.php
+++ resources/views/web/posts.blade.php
@@ -7,6 +7,35 @@
 
 		<h1>Lista de articulos</h1>
 
+		@foreach($posts as $post)
+
+		<div class="panel panel-default">
+			
+			<div class="panel-heading">
+
+				{{ $post->name }}
+
+			</div>
+
+			<div class="panel-body">
+
+				@if($post->file)
+				
+					<img src="{{ $post->file }}" class="img-responsive">
+
+				@endif
+
+				{{ $post->excerpt }}
+				<a href="#" class="pull-right">Leer mas</a>
+
+			</div>
+
+		</div>
+		
+		@endforeach
+
+		{{ $posts->render() }}
+
 	</div>
 
 </div>
